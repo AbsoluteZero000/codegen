e3af1fb79f16e1b71eb90519f20b0a6fe87861ac
Ahmed Wael Wanas <ahmedwaelwanas@gmail.com>
2026-05-08 20:35:27 +0300

    Feat: initial chating

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/absolutezero000/codegen
+
+go 1.25.1
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/revrost/go-openrouter v1.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	openrouter "github.com/revrost/go-openrouter"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	client := openrouter.NewClient(
+		os.Getenv("OPENROUTER_KEY"),
+		openrouter.WithXTitle("My App"),
+		openrouter.WithHTTPReferer("https://myapp.com"),
+	)
+
+	for true {
+		fmt.Printf("User: ")
+		scanner.Scan()
+		message := scanner.Text()
+
+		resp, err := client.CreateChatCompletion(
+			context.Background(),
+			openrouter.ChatCompletionRequest{
+				Messages: []openrouter.ChatCompletionMessage{
+					openrouter.UserMessage(message),
+				},
+			},
+		)
+		if err != nil {
+			fmt.Printf("ChatCompletion error: %v\n", err)
+			return
+		}
+		fmt.Println(resp.Choices[0].Message.Content)
+	}
+
+}
